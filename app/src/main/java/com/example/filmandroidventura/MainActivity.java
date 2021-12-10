656dd283c664d4892ec7b23237cf8aa09a024668
SimonVentura <vsimone474@gmail.com>
2021-12-10 14:45:41 +0100

    situazione secondari

--- app/src/main/java/com/example/filmandroidventura/MainActivity.java
+++ app/src/main/java/com/example/filmandroidventura/MainActivity.java
@@ -5,10 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView lvfilm;
+    Button btnEsci;
+    String film[] = {"spiderman","batman","titanic"}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvfilm = (ListView)findViewById(R.id.lvFilm);
+        ArrayAdapter<String> aaFilm = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,fiml);
+        lvfilm.setAdapter(aaFilm);
+
+        btnEsci = (Button)findViewById(R.id.btnEsci);
+        btnEsci.setOnclickListener(View.OnClickListener()
+        {
+            @override
+            public void onCLick (View view){
+                Toast.makeText(getApplicationContext(),"vuoi uscire", Toast.LENGTH_LONG.show();
+            }
+        });
     }
 }
